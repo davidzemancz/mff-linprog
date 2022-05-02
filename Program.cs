0cc568d738cebe0b1795f19e0196fb76427b7607
David Zeman <david.zeman@outlook.com>
2022-05-02 22:52:53 +0200

    Skeleton

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
 
-namespace Linprog
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+namespace Linprog {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                if (args.Length < 1) { throw new Exception("At least one argument required"); }
+                string firstArg = args[0].Trim().ToLower();
+                if (firstArg == "-h" || firstArg == "--help") { // help
+                    WriteHelpInfo();
+                }
+                else { // input filename
+                    StreamReader streamReader = new StreamReader(firstArg);
+                    string line = streamReader.ReadLine();
+                    if (line.StartsWith("DIGRAPH")) {
+                        while ((line = streamReader.ReadLine()) != null) {
+                            
+                        }
+                    }
+                    else if (line.StartsWith("WEIGHTED DIGRAPH")) {
+                    }
+                }
+
+            } catch(Exception ex) {
+                Console.WriteLine($"[{ex.GetType().Name}] {ex.Message}");
+            }
+        }
+
+        static void WriteHelpInfo() {
+
         }
     }
 }
